f75ba798f0b00c46acfe1ea4f8afe42f92b1a77b
b7c8p <morapaezc@gmail.com>
2021-11-22 22:30:21 -0500

    InitV2

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div>
-      <button className="btn btn-primary">Prueba</button>
+      <button className="btn btn-primary">Prueba Intento repositorio</button>
     </div>
   );
 }
