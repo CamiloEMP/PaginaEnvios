165898f85d9e42f68399d8f60d76a6ab8ae46fc5
b7c8p <morapaezc@gmail.com>
2021-12-02 21:11:04 -0500

    login estilos terminados

--- src/components/FormLogin.jsx
+++ src/components/FormLogin.jsx
@@ -1,5 +1,5 @@
 import "../styles/FormLogin.css";
-
+import { IoCloseCircleOutline } from "react-icons/io5";
 const FormLogin = ({ openModal, setOpenModal }) => {
   const onSubmitLogin = (event) => {
     event.preventDefault();
@@ -12,19 +12,17 @@ const FormLogin = ({ openModal, setOpenModal }) => {
   return (
     <form onClick={onSubmitLogin} className="FLogin">
       <button onClick={closeLogin} className="FLogin-close">
-        X
+        <IoCloseCircleOutline className="icon-close" />
       </button>
-      <h2>DeliveryXpress</h2>
-      <p>IMAGEN</p>
-      <label htmlFor="FLogin-email">
-        Email
-        <input type="email" id="FLogin-email" name="FLogin-email" />
-      </label>
-      <label htmlFor="FLogin-password">
-        Password
-        <input type="password" id="FLogin-password" name="FLogin-password" />
-      </label>
-      <button>Sign in</button>
+      <h3>DeliveryXpress</h3>
+      <div className="Flogin-img-user"></div>
+      <label htmlFor="FLogin-email" className="Flogin-label">Email</label>
+      <input type="email" id="FLogin-email" name="FLogin-email" className="Flogin-input" />
+      <label htmlFor="FLogin-password" className="Flogin-label">Password</label>
+      <input type="password" id="FLogin-password" name="FLogin-password" className="Flogin-input" />
+      <div className="Flogin-container-btn">
+        <button className="Flogin-btn">Sign in</button>
+      </div>
     </form>
   );
 };
