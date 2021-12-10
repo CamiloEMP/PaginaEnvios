1ba57324f921e07280ae9b4451a89450f7889688
jstiwer <jsra65@hotmail.com>
2021-12-09 19:05:24 -0500

    Vistas Stiwer

--- src/App.js
+++ src/App.js
@@ -3,33 +3,24 @@ import { Route, Routes } from "react-router-dom";
 import { PageNotFound } from "./components/Error404/PageNotFound";
 import { UExternPackages } from "./components/UExternPackages";
 import { UInternPackages } from "./components/UInternPackages";
-<<<<<<< HEAD
-import { Home } from "./components/Home";
-=======
 import { GlobalContext } from "./context/Context";
->>>>>>> 46a03e81d761100bc2d8e22c77891dcbbaea4900
 import { HeaderProvisional } from "./HeaderProvisional";
-import {PackegesReception} from "./components/PackegesReception"
+import {PackegesReception} from "./components/PackegesReception";
+import { ScheduledShipments } from "./components/ScheduledShipments";
+import { Home } from "./components/Home";
 function App() {
   return (
     <>
-<<<<<<< HEAD
-      <Routes>
-        <Route exact path="/user-intern" element={<UInternPackages />} />
-        <Route exact path="/Home" element={<Home/>} />
-        <Route exact path="/Package-reception" element={<PackegesReception />} />
-        <Route exact path="/" element={<HeaderProvisional />} />
-      </Routes>
-=======
       <GlobalContext>
         <Routes>
-          <Route exact path="/" element={<HeaderProvisional />} />
+          <Route exact path="/" element={<Home />} />
           <Route exact path="/user-extern" element={<UExternPackages />} />
           <Route exact path="/user-intern" element={<UInternPackages />} />
+          <Route exact path="/ScheduledShipments" element={<ScheduledShipments />} />
+          <Route exact path="/PackegesReception" element={<PackegesReception />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </GlobalContext>
->>>>>>> 46a03e81d761100bc2d8e22c77891dcbbaea4900
     </>
   );
 }
---
+++ src/components/ScheduledShipments.jsx
@@ -0,0 +1,23 @@
+import "../styles/ScheduledShipments.css";
+import { Header } from "./Header"
+
+
+export const ScheduledShipments = ({ props }) => {
+    return (
+        <>
+        <Header> </Header>
+        <div class="C-P">
+            <div class="Title">
+                <h1> Envio programado </h1>
+                <h1> 89765QWE </h1>
+            </div>
+            <div class= "Labels">
+                <p> ahah</p>
+            </div>
+            <div class="Information">
+                <p> asjadsd</p>
+            </div>
+        </div>
+        </>
+
+    )}
