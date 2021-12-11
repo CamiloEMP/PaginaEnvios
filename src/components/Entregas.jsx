fb835437aafc45de44242faf4f05688e66f78a65
TBO57 <emadridr05@gmai.com>
2021-12-10 20:09:05 -0500

    Pequeños ajustes

--- src/components/Entregas.jsx
+++ src/components/Entregas.jsx
@@ -1,18 +1,21 @@
 import { Link } from "react-router-dom";
-/* import Group169 from "../img/Group169"; */
+import { Header } from "./Header";
+import Group169 from "../img/Group169.png";
 import "../styles/Entregados1.css";
+
 export const Entregas = () => {
     return (
       <>
+      <Header />
         <div className="container1">
-            <div className="container-e">
+            <div className="container-Entregas">
                 <div className="titulo">
                     <p>Envios entregados</p>
                 </div>
                 <div className="grande">
-                    <div ClasName="contenedor-t">
+                    <div ClasName="contenedor-tarjetas-Entrega">
                         <div className="imagen-e">
-                            {/* <img src= {Group169} alt="#"/>  */}
+                            <img src= {Group169} alt="Imagen"/>
                         </div>
                         <div className="Descripcion-e">
                             <p>Número de guía: 12455QJE</p>
@@ -23,7 +26,7 @@ export const Entregas = () => {
                     </div>
                     <div classNama="contenedor-t">
                         <div className="imagen-e">
-                            <img src="/Group 169.png" alt="imagen-e" />
+                            <img src= {Group169} alt="Imagen"/>
                         </div>
                         <div className="Descripcion-e">
                             <p>Número de guía: 94321PRE</p>
